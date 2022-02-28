72cf39244898d64feee826b52e7b5eb4d274c101
Umut Işık <umutphp@gmail.com>
2022-02-28 23:18:14 +0300

    Add basic test

---
+++ tests/BasicTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace UmutPHP\BelongsToUser\Tests;
+
+class BasicTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace UmutPHP\BelongsToUser\Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
 
