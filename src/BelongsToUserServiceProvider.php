0ad4e7e6c98b9b261c83513dcea3fabfd2bcdf33
Umut Işık <umutphp@gmail.com>
2022-03-02 00:50:18 +0300

    Add publish to service provider

--- src/BelongsToUserServiceProvider.php
+++ src/BelongsToUserServiceProvider.php
@@ -7,6 +7,18 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class BelongsToUserServiceProvider extends PackageServiceProvider
 {
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/belong-to-user.php' => config_path('belong-to-user.php'),
+        ]);
+    }
+
     public function configurePackage(Package $package): void
     {
         $package->name('belongs-to-user')->hasConfigFile('belongs-to-user');
